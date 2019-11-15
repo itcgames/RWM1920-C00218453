63e4f216314af7402b845430b22af24f1072319d
Quintin Furlong <c00218453@itcarlow.ie>
2019-11-15 11:17:23 +0000

    Displaying done

---
+++ Gun component/Assets/Prefab/Script/BulletMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x++;
+        transform.position = newPosition;
+    }
+}
---
+++ Gun component/Assets/Prefab/Script/FireBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    public GameObject bullet;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 newPosition = new Vector3( transform.position.x +2.4f, transform.position.y +1.4f, transform.position.z );
+            Quaternion newRotation = transform.rotation;
+            Debug.Log(newRotation.z);
+            Instantiate(bullet, newPosition, newRotation);
+        }
+    }
+}

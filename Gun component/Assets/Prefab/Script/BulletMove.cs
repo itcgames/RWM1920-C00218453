978506315c822230267117201ddf1f6092f43535
Quintin Furlong <c00218453@itcarlow.ie>
2019-11-26 12:51:12 +0000

    Bullet has collider

--- Gun component/Assets/Prefab/Script/BulletMove.cs
+++ Gun component/Assets/Prefab/Script/BulletMove.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class BulletMove : MonoBehaviour
 {
+    private void Start()
+    {
+        gameObject.AddComponent<BoxCollider2D>();
+    }
+
     void Update()
     {
         Vector3 newPosition = transform.position;        
         newPosition.x += Mathf.Cos(((transform.rotation.eulerAngles.z + 90) * Mathf.PI) / 180);
         newPosition.y += Mathf.Sin(((transform.rotation.eulerAngles.z + 90) * Mathf.PI) / 180);
         transform.position = newPosition;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "parachute")
+        {
+
+            Destroy(other.gameObject);
+            Debug.Log("OnTriggerEnter2D");
+        }
     }
 }

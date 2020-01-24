d859b7eb9145ba4611ef6f4862d2f21f48ca170e
Quintin Furlong <c00218453@itcarlow.ie>
2020-01-24 10:52:18 +0000

    done f5 tests

---
+++ Gun Component/Assets/Prefab/Script/Tests/TestMultiShot.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestMultiShot
+    {
+        [UnityTest]
+        public IEnumerator TestMultiShot1()
+        {
+            Vector3 startPostion = new Vector3(0, 0, 0);
+            float rotation = 0;
+            Vector3 vectorOne = new Vector3(1, 0, 0);
+            Vector3 vectorTwo = new Vector3(2, 0, 0);
+            float desiredResult = 1.0f;
+
+            Vector3 resultOne = FireBullet.VectorRotation(startPostion, rotation, vectorOne);
+            Vector3 resultTwo = FireBullet.VectorRotation(startPostion, rotation, vectorTwo);
+            float result = Vector3.Distance(resultOne, resultTwo);
+
+            Assert.AreEqual(result, desiredResult);
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator TestMultiShot2()
+        {
+            Vector3 startPostion = new Vector3(0, 0, 0);
+            float rotation = 77;
+            Vector3 vectorOne = new Vector3(1, 0, 0);
+            Vector3 vectorTwo = new Vector3(2, 0, 0);
+            float desiredResult = 1.0f;
+
+            Vector3 resultOne = FireBullet.VectorRotation(startPostion, rotation, vectorOne);
+            Vector3 resultTwo = FireBullet.VectorRotation(startPostion, rotation, vectorTwo);
+            float result = Vector3.Distance(resultOne, resultTwo);
+            float temp = Mathf.Ceil(result);
+            if (result - temp > 0.999)
+            {
+                result = temp + 1;
+            }
+            else if (result - temp < 0.001)
+            {
+                result = temp;
+            }
+            Assert.AreEqual(result, desiredResult);
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator TestMultiShot3()
+        {
+            Vector3 startPostion = new Vector3(2, -3, 0);
+            float rotation = 0;
+            Vector3 vectorOne = new Vector3(1, 0, 0);
+            Vector3 vectorTwo = new Vector3(2, 0, 0);
+            float desiredResult = 1.0f;
+
+            Vector3 resultOne = FireBullet.VectorRotation(startPostion, rotation, vectorOne);
+            Vector3 resultTwo = FireBullet.VectorRotation(startPostion, rotation, vectorTwo);
+            float result = Vector3.Distance(resultOne, resultTwo);
+            
+            Assert.AreEqual(result, desiredResult);
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator TestMultiShot4()
+        {
+            Vector3 startPostion = new Vector3(0, 0, 0);
+            float rotation = 0;
+            Vector3 vectorOne = new Vector3(3, 0, 0);
+            Vector3 vectorTwo = new Vector3(0, 4, 0);
+            float desiredResult = 5.0f;
+
+            Vector3 resultOne = FireBullet.VectorRotation(startPostion, rotation, vectorOne);
+            Vector3 resultTwo = FireBullet.VectorRotation(startPostion, rotation, vectorTwo);
+            float result = Vector3.Distance(resultOne, resultTwo);
+
+            Assert.AreEqual(result, desiredResult);
+            yield return null;
+        }
+    }
+}
--- Gun component/Assets/Prefab/Script/FireBullet.cs
+++ Gun component/Assets/Prefab/Script/FireBullet.cs
@@ -33,13 +33,10 @@ public class FireBullet : MonoBehaviour
                 Vector3 vector2 = new Vector3(1.8f * setScale.GUN_SCALE, 1.9f * setScale.GUN_SCALE, 0.0f);
                 Vector3 vector3 = new Vector3(1.1f * setScale.GUN_SCALE, 1.9f * setScale.GUN_SCALE, 0.0f);
                 Quaternion newRotation = transform.rotation;
-                vector = Quaternion.Euler(0, 0, newRotation.eulerAngles.z) * vector;
-                vector2 = Quaternion.Euler(0, 0, newRotation.eulerAngles.z) * vector2;
-                vector3 = Quaternion.Euler(0, 0, newRotation.eulerAngles.z) * vector3;
 
-                Vector3 newPosition = new Vector3(transform.position.x + vector.x, transform.position.y + vector.y, 1);
-                Vector3 newPosition2 = new Vector3(transform.position.x + vector2.x, transform.position.y + vector2.y, 1);
-                Vector3 newPosition3 = new Vector3(transform.position.x + vector3.x, transform.position.y + vector3.y, 1);
+                Vector3 newPosition = VectorRotation(transform.position, newRotation.eulerAngles.z, vector);
+                Vector3 newPosition2 = VectorRotation(transform.position, newRotation.eulerAngles.z, vector2);
+                Vector3 newPosition3 = VectorRotation(transform.position, newRotation.eulerAngles.z, vector3);
 
                 newRotation = Quaternion.Euler(0, 0, newRotation.eulerAngles.z - 90.0f);
                 Instantiate(bullet, newPosition, newRotation);
@@ -47,11 +44,9 @@ public class FireBullet : MonoBehaviour
                 if (numOfShots > 1)
                 {
                     Instantiate(bullet, newPosition2, newRotation);
-                    audioData.Play(0);
                     if (numOfShots > 2)
                     {
                         Instantiate(bullet, newPosition3, newRotation);
-                        audioData.Play(0);
                     }
                 }
                 
@@ -62,5 +57,10 @@ public class FireBullet : MonoBehaviour
                 start = false;
             }
         }
+    }
+    public static Vector3 VectorRotation(Vector3 t_startPos, float t_rotation, Vector3 t_vector)
+    {
+        t_vector = Quaternion.Euler(0, 0, t_rotation) * t_vector; 
+        return new Vector3(t_startPos.x + t_vector.x, t_startPos.y + t_vector.y, 1);
     }
 }
